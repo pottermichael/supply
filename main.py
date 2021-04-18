55221f01633e3786f906aff3715f8133e8ef1ce4
pottermichael <potter.michael@gmail.com>
2021-04-18 09:29:06 -0500

    added isos to chicago

--- main.py
+++ main.py
@@ -56,8 +56,16 @@ def denver_cbd():
     print(data)
     return render_template("denver_cbd.html",data=data)
 
+def get_isos():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", "cta_5_min.geojson")
+    data = json.load(open(json_url))
+    return data
+
 @main.route('/chicago', methods=["GET"])
 def chicago():
+    isos = get_isos()
+    print(isos)
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static/data", "chicago_supply_final.geojson")
     data = json.load(open(json_url))
@@ -67,7 +75,7 @@ def chicago():
     #print(gdf)
     print(type(data))
     print(data)
-    return render_template("chicago.html",data=data)
+    return render_template("chicago.html",data=data, isos=isos)
 
 @main.route('/geo', methods=["GET"])
 def geojson_to_gpd():
